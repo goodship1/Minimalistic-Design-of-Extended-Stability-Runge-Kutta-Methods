7942a73c61ca6833fc4cec165f5b67633b3a494f
Gavin Goodship <msgoodship1@gmail.com>
2025-05-09 17:23:29 +0100

    Update 2Dbrusselator.py

--- 2Dbrusselator.py
+++ 2Dbrusselator.py
@@ -6,7 +6,7 @@ import tracemalloc
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
-# Configure NumPy to raise exceptions on floating-point errors
+# Configure NumPy to raise exceptions on floating-point errorsa
 np.seterr(over='raise', invalid='raise', divide='raise', under='warn')
 
 def kahan_sum(values):
